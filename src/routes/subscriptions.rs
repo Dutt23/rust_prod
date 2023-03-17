9b3b7ff11ec1a9cad6830975cf2c3d0ab71dff43
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-03-17 16:26:23 +0530

    Add a span

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,24 @@ struct FormData {
 
 #[post("/subscriptions")]
 async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    let request_id = Uuid::new_v4();
+    // Spans, like logs, have an associated level // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!(
+        "Adding '{}' and '{}' as a new subscriber",
+        form.email,
+        form.name
+    );
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
@@ -25,9 +43,12 @@ async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(err) => {
-            print!("Error occured while saving : {}", err);
+            tracing::error!("Error occured while saving : {}", err);
             HttpResponse::InternalServerError().finish()
         }
     }
