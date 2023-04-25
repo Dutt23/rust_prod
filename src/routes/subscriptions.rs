9e34bd9f62cd7020c395ce6e2682aceac8b46fad
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-04-25 15:23:22 +0530

    Use macros

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,54 +1,20 @@
 use actix_web::{post, web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
-struct FormData {
+pub struct FormData {
     email: String,
     name: String,
 }
 
+// Spans, like logs, have an associated level // `info_span` creates a span at the info-level
+// See the following section on `Instrumenting Futures`
+#[tracing::instrument(name = "Adding a new subscriber.", skip(form, pool), fields(request_id = %Uuid::new_v4(), subscriber_email = %form.email, subscriber_name= %form.name))]
 #[post("/subscriptions")]
 async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
-    let request_id = Uuid::new_v4();
-    // Spans, like logs, have an associated level // `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name= %form.name
-    );
-    // Using `enter` in an async function is a recipe for disaster!
-    // Bear with me for now, but don't do this at home.
-    // See the following section on `Instrumenting Futures`
-    let _request_span_guard = request_span.enter();
-
-    let query_span = tracing::info_span!("Saving user details", 
-     %request_id,
-    subscriber_email = %form.email,
-    subscriber_name= %form.name);
-    tracing::info!(
-        "Adding '{}' and '{}' as a new subscriber",
-        form.email,
-        form.name
-    );
-    match sqlx::query!(
-        r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
-    "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now()
-    )
-    // We use `get_ref` to get an immutable reference to the `PgConnection`
-    // wrapped by `web::Data`.
-    .execute(pool.get_ref())
-    .instrument(query_span)
-    .await
-    {
+    match insert(&pool, &form).await {
         Ok(_) => {
             tracing::info!("New subscriber details have been saved");
             HttpResponse::Ok().finish()
@@ -58,4 +24,30 @@ async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
             HttpResponse::InternalServerError().finish()
         }
     }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+   INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
+   "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(pool)
+    .await
+    .map_err(|err| {
+        tracing::error!("Error happened while executing query :{:?}", err);
+        err
+    })?;
+
+    Ok(())
 }
