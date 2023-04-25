3067549fb06577fd0b7d5c8714181ac1156e1243
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-04-25 14:21:25 +0530

    Clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 use news_letter::{configuration::get_configuration, startup::*};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
+use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+
+    let formatting_layer = BunyanFormattingLayer::new(
+        name,
+        // Output the formatted spans to stdout.
+        std::io::stdout,
+    );
+
+    // The `with` method is provided by `SubscriberExt`, an extension // trait for `Subscriber` exposed by `tracing_subscriber`
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -16,22 +38,7 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(format!("127.0.0.1:{}", settings.application_port))?;
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "news_letter".into(),
-        // Output the formatted spans to stdout.
-        std::io::stdout,
-    );
-
-    // The `with` method is provided by `SubscriberExt`, an extension // trait for `Subscriber` exposed by `tracing_subscriber`
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    // `set_global_default` can be used by applications to specify
-    // what subscriber should be used to process spans.
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("news_letter".into(), "info".into());
+    init_subscriber(subscriber);
     run(listener, connection_pool)?.await
 }
