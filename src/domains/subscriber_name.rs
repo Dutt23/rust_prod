326ce741d6cef06a439cea1b9b8a436b55251e5d
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-04-25 21:52:11 +0530

    Change domain type structures

---
+++ src/domains/subscriber_name.rs
@@ -0,0 +1,61 @@
+#[derive(Debug)]
+pub struct SubscriberName(String);
+use unicode_segmentation::UnicodeSegmentation;
+
+impl SubscriberName {
+    pub fn parse(name: String) -> Result<SubscriberName, String> {
+        let is_empty_string = name.trim().is_empty();
+        let is_too_long = name.graphemes(true).count() > 256;
+        let forbidden_chars = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let forbidden_char_present = name.chars().any(|char| forbidden_chars.contains(&char));
+
+        if is_empty_string || is_too_long || forbidden_char_present {
+            Err(format!("{} is not a valid subscriber name", name))
+        } else {
+            Ok(Self(name))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domains::subscriber_name::SubscriberName;
+    use claims::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_graphene_long_name_is_valid() {
+        let name = "a".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_then_256_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn white_spaced_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_invalid_character_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            assert_err!(SubscriberName::parse(name.to_string()));
+        }
+    }
+
+    #[test]
+    fn valid_name_parsed_successfully() {
+        let name = "Valid name".to_string();
+        assert_ok!(SubscriberName::parse(name.to_string()));
+    }
+}
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,7 +3,7 @@ use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domains::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -16,9 +16,14 @@ pub struct FormData {
 #[tracing::instrument(name = "Adding a new subscriber.", skip(form, pool), fields(subscriber_email = %form.email, subscriber_name= %form.name))]
 #[post("/subscriptions")]
 async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name),
+        name: name,
     };
 
     match insert(&pool, &new_subscriber).await {
