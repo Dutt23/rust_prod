3d32e1d6e921deab276627e67aba630bc6814f15
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-08-03 23:28:28 +0530

    Add code to store subscription token

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,13 +1,14 @@
-use actix_web::{post, web, HttpResponse, Responder};
-use chrono::Utc;
-use sqlx::PgPool;
-use uuid::Uuid;
-
 use crate::{
     domains::{NewSubscriber, SubscriberEmail, SubscriberName},
     email_client::EmailClient,
     startup::ApplicationBaseUrl,
 };
+use actix_web::{post, web, HttpResponse, Responder};
+use chrono::Utc;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -25,6 +26,14 @@ impl TryFrom<FormData> for NewSubscriber {
     }
 }
 
+fn generate_subscription_token() -> String {
+    let mut rng = thread_rng();
+    std::iter::repeat_with(|| rng.sample(Alphanumeric))
+        .map(char::from)
+        .take(25)
+        .collect()
+}
+
 // Spans, like logs, have an associated level // `info_span` creates a span at the info-level
 // See the following section on `Instrumenting Futures`
 #[tracing::instrument(name = "Adding a new subscriber.", skip(form, pool,  email_client, base_url), fields(subscriber_email = %form.email, subscriber_name= %form.name))]
@@ -40,15 +49,29 @@ async fn subscriptions(
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
 
-    if insert(&pool, &new_subscriber).await.is_err() {
+    let subscription_token = generate_subscription_token();
+
+    let subscription_id = match insert(&pool, &new_subscriber).await {
+        Ok(subscription_id) => subscription_id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    if store_token(&pool, &subscription_id, &subscription_token)
+        .await
+        .is_err()
+    {
         return HttpResponse::InternalServerError().finish();
     }
 
     tracing::info!("New subscriber details have been saved");
-
-    if send_confirmation_email_to_customer(&email_client, new_subscriber, &base_url.0, "mytoken")
-        .await
-        .is_err()
+    if send_confirmation_email_to_customer(
+        &email_client,
+        new_subscriber,
+        &base_url.0,
+        &subscription_token,
+    )
+    .await
+    .is_err()
     {
         HttpResponse::InternalServerError().finish();
     }
@@ -63,7 +86,7 @@ async fn send_confirmation_email_to_customer(
     email_client: &EmailClient,
     new_subscriber: NewSubscriber,
     base_url: &String,
-    subscription_token: &str,
+    subscription_token: &String,
 ) -> Result<(), reqwest::Error> {
     let confirmation_link =
         &format!("{base_url}/subscriptions/confirm?subscription_token={subscription_token}");
@@ -85,12 +108,13 @@ async fn send_confirmation_email_to_customer(
     name = "Saving new subscriber details in the database",
     skip(new_subscriber, pool)
 )]
-pub async fn insert(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
+pub async fn insert(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<Uuid, sqlx::Error> {
+    let subscription_id = Uuid::new_v4();
     sqlx::query!(
         r#"
    INSERT INTO subscriptions (id, email, name, subscribed_at, status) VALUES ($1, $2, $3, $4, 'pending_confirmation')
    "#,
-        Uuid::new_v4(),
+   subscription_id,
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
@@ -104,5 +128,28 @@ pub async fn insert(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(),
         err
     })?;
 
+    Ok((subscription_id))
+}
+
+#[tracing::instrument(
+    name = "Storing subscription token in the database",
+    skip(subscription_id, pool)
+)]
+pub async fn store_token(
+    pool: &PgPool,
+    subscription_id: &Uuid,
+    subscription_token: &String,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"INSERT into subscription_tokens (subscription_token, subscription_id) VALUES ($1, $2)"#,
+        subscription_token,
+        subscription_id
+    )
+    .execute(pool)
+    .await
+    .map_err(|err| {
+        tracing::error!("Error happened while executing query :{:?}", err);
+        err
+    })?;
     Ok(())
 }
