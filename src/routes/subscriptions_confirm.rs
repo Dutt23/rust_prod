69d5835275ff9b072b202c569263f464523a6e26
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-08-03 23:51:55 +0530

    Add code to confirm subscription

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,5 +1,5 @@
 use actix_web::{get, web, HttpResponse};
-use sqlx::{pool, PgPool};
+use sqlx::PgPool;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,7 +10,43 @@ pub struct Parameters {
 #[tracing::instrument(name = "Confirming a subscriber", skip(_parameters))]
 #[get("/subscriptions/confirm")]
 pub async fn confirm(_parameters: web::Query<Parameters>, pool: web::Data<PgPool>) -> HttpResponse {
+    let subscription_id =
+        match get_subscription_id_from_token(&pool, &_parameters.subscription_token).await {
+            Ok(subscription_id) => subscription_id,
+            Err(_) => return HttpResponse::InternalServerError().finish(),
+        };
+
+    match subscription_id {
+        None => return HttpResponse::InternalServerError().finish(),
+        Some(subscription_id) => {
+            if confirm_subscription_token(&pool, &subscription_id)
+                .await
+                .is_err()
+            {
+                return HttpResponse::InternalServerError().finish();
+            }
+        }
+    }
+
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(name = "Confirming a subscription in the database", skip(pool))]
+pub async fn confirm_subscription_token(
+    pool: &PgPool,
+    subscription_id: &Uuid,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"UPDATE subscriptions SET status = 'confirmed' WHERE id = $1"#,
+        subscription_id
+    )
+    .execute(pool)
+    .await
+    .map_err(|err| {
+        tracing::error!("Failed to execute query: {:?}", err);
+        err
+    })?;
+    Ok(())
 }
 
 #[tracing::instrument(
