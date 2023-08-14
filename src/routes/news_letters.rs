ad17bca9ba29378b85d770bac1f1f8b765300e5e
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-08-14 21:12:46 +0530

    Fix response params

--- src/routes/news_letters.rs
+++ src/routes/news_letters.rs
@@ -1,5 +1,9 @@
-use actix_web::{post, web, HttpResponse};
+use actix_web::{post, web, HttpResponse, ResponseError};
+use reqwest::StatusCode;
 use sqlx::{PgPool, Postgres, Transaction};
+
+use anyhow::Context;
+
 #[derive(serde::Deserialize)]
 pub struct NewsLetter {
     title: String,
@@ -16,14 +20,28 @@ struct ConfirmedSubscriber {
     email: String,
 }
 
+#[derive(thiserror::Error, Debug)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnExceptedError(#[from] anyhow::Error),
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> reqwest::StatusCode {
+        match self {
+            PublishError::UnExceptedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
 #[tracing::instrument(name = "Publish a news letter", skip(_body))]
 #[post("/newsletter")]
 pub async fn publish_newsletter(
     _body: web::Json<NewsLetter>,
     pool: web::Data<PgPool>,
-) -> HttpResponse {
-    let _subscribers = get_confirmed_subscribers(&pool).await;
-    return HttpResponse::Ok().finish();
+) -> Result<HttpResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(name = "Get a list of confirmed subscribers", skip(pool))]
