80e44aaf08b1a0dde4dafe0a0b659235500f7598
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-08-17 19:20:18 +0530

    Add fields to current span

--- src/routes/news_letters.rs
+++ src/routes/news_letters.rs
@@ -5,7 +5,7 @@ use actix_web::{
 };
 use base64::{engine::general_purpose, Engine as _};
 use reqwest::StatusCode;
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
 //  format! allocates memory on the heap to store its output
@@ -71,6 +71,7 @@ impl ResponseError for PublishError {
 #[tracing::instrument(
     name = "Publish a news letter",
     skip(news_letter, email_client, pool, request)
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
 #[post("/newsletter")]
 pub async fn publish_newsletter(
@@ -79,9 +80,12 @@ pub async fn publish_newsletter(
     email_client: web::Data<EmailClient>,
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
     let subscribers = get_confirmed_subscribers(&pool).await?;
+    let user_id = validate_credentials(&credentials, &pool).await?;
 
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
     for subscriber in subscribers {
         match subscriber {
             Ok(email) => email_client
@@ -119,6 +123,27 @@ async fn get_confirmed_subscribers(
     .iter()
     .map(|row| SubscriberEmail::parse(row.email.clone()))
     .collect())
+}
+
+#[tracing::instrument(name = "Validating user credentials", skip(credentials, pool))]
+pub async fn validate_credentials(
+    credentials: &Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id = sqlx::query!(
+        r#"SELECT user_id from users where username = $1 AND password = $2"#,
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to perform query for auth user")
+    .map_err(PublishError::UnExceptedError)?;
+
+    user_id
+        .map(|row| row.user_id)
+        .ok_or_else(|| anyhow::anyhow!("Invalid username or password !"))
+        .map_err(PublishError::AuthError)
 }
 
 pub fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
