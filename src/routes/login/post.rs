05319ce448983e872c0660022683d9fd97f99ca8
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-08-24 14:41:32 +0530

    Handle error

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,9 +1,61 @@
-use actix_web::{post, HttpResponse};
-use reqwest::header::LOCATION;
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    routes::error_chain_fmt,
+};
+use actix_web::{post, web, HttpResponse, ResponseError};
+use reqwest::{header::LOCATION, StatusCode};
+use secrecy::Secret;
+use sqlx::PgPool;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
 
 #[post("/login")]
-pub async fn login() -> HttpResponse {
-    HttpResponse::SeeOther()
+pub async fn login(
+    form_data: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, LoginError> {
+    let credentials = Credentials {
+        username: form_data.0.username,
+        password: Secret::new(form_data.0.password),
+    };
+
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentialsError(_) => LoginError::AuthError(e.into()),
+            AuthError::UnExceptedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
+    Ok(HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
-        .finish()
+        .finish())
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
