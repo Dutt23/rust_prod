971ed870122abe796873fb203337d8f61dc53293
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-10-05 10:46:34 +0530

    Add types session

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,5 @@ pub mod domains;
 pub mod email_client;
 pub mod routes;
 pub mod startup;
+pub mod state_session;
 pub mod telemetry;
---
+++ src/state_session.rs
@@ -0,0 +1,18 @@
+use actix_session::{Session, SessionGetError, SessionInsertError};
+use uuid::Uuid;
+pub struct TypedSession(Session);
+
+impl TypedSession {
+    const USER_ID: &'static str = "user_id";
+
+    pub fn renew(&self) {
+        self.0.renew();
+    }
+
+    pub fn insert_user_id(&self, user_id: Uuid) -> Result<(), SessionInsertError> {
+        self.0.insert(Self::USER_ID, user_id)
+    }
+    pub fn get_user_id(&self) -> Result<Option<Uuid>, SessionGetError> {
+        self.0.get(Self::USER_ID)
+    }
+}
