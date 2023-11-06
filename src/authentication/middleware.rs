d35d58baa4f718ff286cd63682ed24d9a8055c6a
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2023-11-07 01:20:22 +0530

    Add middleware for auth

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -1,3 +1,34 @@
-pub async fn reject_anonymous_users() {
-    todo!()
+use crate::{routes::admin::dashboard::e500, state_session::TypedSession};
+use actix_web::{
+    body::MessageBody,
+    dev::{ServiceRequest, ServiceResponse},
+    error::InternalError,
+    FromRequest, HttpResponse,
+};
+use actix_web_lab::middleware::Next;
+use reqwest::header::LOCATION;
+
+fn see_other(route: &str) -> HttpResponse {
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, route))
+        .finish()
+}
+
+pub async fn reject_anonymous_users(
+    mut req: ServiceRequest,
+    next: Next<impl MessageBody>,
+) -> Result<ServiceResponse<impl MessageBody>, actix_web::Error> {
+    let session = {
+        let (http_request, payload) = req.parts_mut();
+        TypedSession::from_request(http_request, payload).await
+    }?;
+
+    match session.get_user_id().map_err(e500)? {
+        Some(_) => next.call(req).await,
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("The user has not logged in");
+            Err(InternalError::from_response(e, response).into())
+        }
+    }
 }
