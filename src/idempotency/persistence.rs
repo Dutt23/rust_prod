fb4dca795cf2c725001f7a6ccbaff42d986caf2c
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2024-01-30 21:33:04 +0530

    Add persistence layer

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -1,8 +1,16 @@
 use super::IdempotencyKey;
+use actix_web::body::to_bytes;
 use actix_web::HttpResponse;
 use reqwest::StatusCode;
+use sqlx::postgres::PgHasArrayType;
 use sqlx::PgPool;
 use uuid::Uuid;
+
+impl PgHasArrayType for HeaderPairRecord {
+    fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+        sqlx::postgres::PgTypeInfo::with_name("_header_pair")
+    }
+}
 
 #[derive(Debug, sqlx::Type)]
 #[sqlx(type_name = "header_pair")]
@@ -34,3 +42,29 @@ pub async fn get_saved_response(
         Ok(None)
     }
 }
+
+pub async fn save_response(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+    http_response: HttpResponse,
+) -> Result<HttpResponse, anyhow::Error> {
+    let status_code = http_response.status().as_u16() as i16;
+    let (response_head, body) = http_response.into_parts();
+    let headers = {
+        let mut h = Vec::with_capacity(response_head.headers().len());
+        for (name, value) in response_head.headers().iter() {
+            let name = name.as_str().to_owned();
+            let value = value.as_bytes().to_owned();
+            h.push(HeaderPairRecord { name, value })
+        }
+        h
+    };
+    let body = to_bytes(body).await.map_err(|e| anyhow::anyhow!("{}", e))?;
+    sqlx::query_unchecked!(r#"INSERT INTO idempotency (user_id, idempotency_key, response_status_code, response_headers,  response_body, created_at) VALUES ($1, $2, $3, $4, $5, now())"#, user_id, idempotency_key.as_ref(), status_code, headers, body.as_ref()).execute(pool)
+    .await?;
+    let http_response = response_head.set_body(body).map_into_boxed_body();
+    Ok(http_response)
+}
+
+fn test(a: &mut i32) {}
